2aa0e5656caff0960e93323e092d8def36d4fc55
Shivanshu Verma <shivanshu_verma@Tycorra.local>
2023-04-18 13:00:21 -0400

    new

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
  const port = process.env.PORT  || 5000;
@@ -7,8 +8,16 @@ const app = express();
 
 //Enable body parser
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Set Routes
+
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//Set Static Folder
 
 
 app.use('/openai', require('./routes/openaiROutes'));
